65d275c53600912128d370ed7b3a8f11719d6898
Ziencia <alberto.madrid.espana@gmail.com>
2025-05-01 16:47:59 +0200

    Mejora visual navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,30 +1,41 @@
 <template>
-     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <router-link class="nav-link" to="/home">INICIO</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/clientes">Clientes</router-link>
-            </li>            
-            <li class="nav-item">
-              <router-link class="nav-link" to="/vehiculos">Vehiculos</router-link>
-            </li>         
-            <li class="nav-item">
-              <router-link class="nav-link" to="/transacciones">Transacciones</router-link>
-            </li>       
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+    <div class="container-fluid">
+      <router-link class="navbar-brand fs-4 d-flex align-items-center" to="/home">
+        <i class="bi bi-house-door-fill me-2"></i> INICIO
+      </router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
+          <li class="nav-item">
+            <router-link class="nav-link d-flex align-items-center" to="/clientes">
+              <i class="bi bi-people-fill me-2"></i> Clientes
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link d-flex align-items-center" to="/vehiculos">
+              <i class="bi bi-truck-front-fill me-2"></i> Vehículos
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link d-flex align-items-center" to="/transacciones">
+              <i class="bi bi-cash-stack me-2"></i> Transacciones
+            </router-link>
+          </li>
+        </ul>
       </div>
-
- </nav>
- </template>
-
-<style scoped>
-backgr {
-  color: #888;
-}
-</style>
+    </div>
+  </nav>
+</template>
