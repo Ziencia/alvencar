57d030174778eed05631c180fa782568b48f5f69
Ziencia <alberto.madrid.espana@gmail.com>
2025-06-02 22:58:07 +0200

    Se habia eliminado por error en algun merge el  import de un metodo

--- frontend/src/stores/transaccionStore.js
+++ frontend/src/stores/transaccionStore.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { defineStore } from 'pinia';
-import { getTransacciones, postFactura, deleteTransaccionPorId } from './api-service';
+import { getTransacciones, postFactura, deleteTransaccionPorId, updateVehiculo } from './api-service';
 
 export const useTransaccionStore = defineStore('transaccion', {
   state() {
